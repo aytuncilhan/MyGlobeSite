b468c12c7012ec4bd8dfe3c9a277a4a91a2916de
Aytunc Ilhan <16980064+aytuncilhan@users.noreply.github.com>
2023-06-18 21:38:41 +0200

    fix

--- Assets/Scripts/crwl.py
+++ Assets/Scripts/crwl.py
@@ -19,52 +19,12 @@ file_path = 'Assets/JobsLib/jobs.json'
 exisitng_jobs = readContent(repo_owner, repo_name, access_token, file_path)
 
 # Crawl the Job Website
-# url = 'https://nato.taleo.net/careersection/2/jobsearch.ftl?lang=en'
-# response = requests.get(url)
-# html_content = response.text
-# soup = BeautifulSoup(html_content, 'html.parser')
+url = 'https://nato.taleo.net/careersection/2/jobsearch.ftl?lang=en'
+response = requests.get(url)
+html_content = response.text
+soup = BeautifulSoup(html_content, 'html.parser')
 
-# retrieved_jobs = scrapePage(soup)
-
-# with open('output.txt', 'w') as file:
-#     file.write(soup.prettify())
-
-
-
-from selenium import webdriver
-from selenium.webdriver.support.ui import Select
-
-# Create a WebDriver instance (assuming you have Chrome WebDriver installed)
-driver = webdriver.Chrome()
-
-# Load the page
-driver.get('https://nato.taleo.net/careersection/2/jobsearch.ftl?lang=en')
-
-# Find the <select> element by its ID
-select_element = driver.find_element('css selector', '#requisitionListInterface\\.dropListSize')
-
-# Select the option with value="100"
-select = Select(select_element)
-select.select_by_value('100')
-
-# Fetch the updated page source
-updated_html = driver.page_source
-
-with open('output_updated.txt', 'w') as file:
-    file.write(updated_html)
-
-
-# Create a BeautifulSoup object from the updated HTML
-soup2 = BeautifulSoup(updated_html, 'html.parser')
-
-with open('output_updated_soup.txt', 'w') as file:
-    file.write(soup2.prettify())
-
-retrieved_jobs = scrapePage(soup2)
-
-# Close the browser
-driver.quit()
-
+retrieved_jobs = scrapePage(soup)
 
 # Create the arrays for the HTML table
 publishDate = []
@@ -85,5 +45,3 @@ encoded_html = base64.b64encode(table_html.encode()).decode()
 
 # Update Github repo with the ads.html and jobs.json
 writeContent(repo_owner, repo_name, access_token, branch_name, encoded_html, retrieved_jobs)
-
-## END OF CODE
